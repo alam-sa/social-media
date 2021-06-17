2e0f38d5adcc747f73e3ae6cf6a321a49b537f60
Andi Samsualam <andi.samsualam95@gmail.com>
2021-06-17 22:08:45 +0800

    add post and timeline

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
-const router = require('express').Router()
-const userRouter = require('./users')
-const authRouter = require('./auth')
+const router = require('express').Router();
+const userRouter = require('./users');
+const authRouter = require('./auth');
+const postRouter = require("./posts")
 
-router.use('/api/users', userRouter)
-router.use('/api/auth', authRouter)
+router.use('/api/users', userRouter);
+router.use('/api/auth', authRouter);
+router.use('/api/posts', postRouter);
 
 module.exports = router 
--- routes/users.js
+++ routes/users.js
@@ -71,6 +71,28 @@ router.put("/:id/follow", async (req, res) => {
   } else {
     res.status(403).json("you can't follow yourself");
   }
+});
+
+// UNFOLLOW USER
+router.put("/:id/unfollow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    console.log("masukk");
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser =  await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({ $pull: { followings: req.body.userId } });
+        res.status(200).json("user has been unfollowed");
+      } else {
+        res.status(403).json("you don't follow this user");
+      }
+    } catch(err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("you can't unfollow yourself");
+  }
 })
 
 
